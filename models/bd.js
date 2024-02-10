bad08669cbe2646a6a1c5c8cc3f31b38ee6d7188
Gonzalo Lopolito <gonzalo@magnetico.dev>
2024-02-10 15:10:50 -0300

    install mysql2

--- models/bd.js
+++ models/bd.js
@@ -1,13 +1,13 @@
-var mysql = require("mysql");
+var mysql = require("mysql2/promise");
 var util = require("util");
 
 var pool = mysql.createPool({
   connectionLimit: 10,
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DB_NAME,
-  port: process.env.MYSQL_PORT
+  host: process.env.MYSQL_HOST || 'localhost',
+  user: process.env.MYSQL_USER || 'root',
+  password: process.env.MYSQL_PASSWORD || 'gonza1123',
+  database: process.env.MYSQL_DB_NAME || 'db_2uapp',
+  port: process.env.MYSQL_PORT || 3306
 });
 
 pool.query = util.promisify(pool.query);
